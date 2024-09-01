b6547aa47eee611dc4918c65937947c565e4c159
Andrew Pruski <dbafromthecold@gmail.com>
2024-09-01 11:35:23 +0100

    fixed line endings in demo scripts

--- demos/compose/01-update_hba_conf.sh
+++ demos/compose/01-update_hba_conf.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-
 # Append the configuration line to pg_hba.conf
 echo "host replication replicator 172.18.0.1/24 trust" >> /var/lib/postgresql/data/pg_hba.conf
--- demos/compose/03-base_backup.sh
+++ demos/compose/03-base_backup.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-
 # Set the password to connect to the instance
 export PGPASSWORD=Testing1122
-
 
 # take a base backup
 pg_basebackup -D /postgres/archive/base --checkpoint=fast -S replication_slot_slave1 -X stream -U replicator -Fp -R
 
-
 # Create a flag file to indicate that the base backup is complete
 touch /postgres/archive/base/ready.flag
 
-
-# Change the owner of the base backup directory to the postgres user
+# Change permissions on the backup directory
 chmod 700 -R /postgres/archive/base
-
 
 # Restart PostgreSQL to apply changes
 /usr/lib/postgresql/16/bin/pg_ctl -D /var/lib/postgresql/data restart
