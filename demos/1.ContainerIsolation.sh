9539a609dafbe9471f7fda4fc56f0ce688fe6a60
Andrew Pruski <dbafromthecold@gmail.com>
2024-10-29 11:28:27 +0000

    updated demos

--- demos/1.ContainerIsolation.sh
+++ demos/1.ContainerIsolation.sh
@@ -14,12 +14,11 @@
 
 # run a container - limiting the memory available to it
 docker container run -d \
---publish 15789:1433 \
+--name postgres1 \
+--publish 15793:5432 \
 --memory 2048M \
---env ACCEPT_EULA=Y \
---env MSSQL_SA_PASSWORD=Testing1122 \
---name sqlcontainer1 \
-mcr.microsoft.com/mssql/server:2019-CU5-ubuntu-18.04
+--env POSTGRES_PASSWORD=Testing1122 \
+ghcr.io/dbafromthecold/custom-postgres:latest
 
 
 
@@ -65,7 +64,7 @@ cat $CPUCGROUP/cpu.cfs_quota_us
 
 
 # set CPU limit on the fly - thanks Anthony!
-docker update sqlcontainer1 --cpus 2
+docker update postgres1 --cpus 2
 
 
 
@@ -74,9 +73,8 @@ cat $CPUCGROUP/cpu.cfs_quota_us
 
 
 
-
 # view namespaces created
-sudo lsns | grep mssql
+sudo lsns | grep postgres
 
 
 
@@ -86,22 +84,22 @@ hostname
 
 
 # view the hostname within the container
-docker exec sqlcontainer1 hostname
+docker exec postgres1 hostname
 
 
 
 # list processes within the container
-docker exec sqlcontainer1 ps aux
+docker exec postgres1 ps aux
 
 
 
-# view mssql processes on host
-ps aux | grep mssql
+# view postgres processes on host
+ps aux | grep postgres
 
 
 
 # grab the pid
-pid=$(sudo lsns | grep mssql | awk '!visited[$4]++ {print $4}') && echo $pid
+pid=$(sudo lsns | grep postgres | awk '!visited[$4]++ {print $4}') && echo $pid
 
 
 
@@ -125,194 +123,35 @@ exit
 
 
 
-# let's run another container from a custom image which runs SQL as root
-docker container run -d \
---publish 15799:1433 \
---env ACCEPT_EULA=Y \
---env MSSQL_SA_PASSWORD=Testing1122 \
---name sqlcontainer2 \
-ghcr.io/dbafromthecold/dockerdeepdive:customsql2019-root
-
-
-
-# view mssql processes on host again
-ps aux | grep mssql
+# set environmant variable for password to connect to postgres
+export PGPASSWORD='Testing1122'
 
 
 
 # create a database in the SQL instance in the container
-mssql-cli -S localhost,15789 -U sa -P Testing1122 -Q "CREATE DATABASE [testdatabase]"
+psql -h localhost -p 15793 -U postgres -d postgres -c "CREATE DATABASE testdatabase"
+
+
+
+# confirm database has been created
+psql -h localhost -p 15793 -U postgres -d postgres -l
 
 
 
 # list the database files within the container
-docker exec sqlcontainer1 ls -al /var/opt/mssql/data
+docker exec postgres1 ls -al /var/lib/postgresql/data/base
 
 
 
-# list those files on the host
-ls -al /var/opt/mssql/data
+# get container's volume location on host
+FILES=$(docker inspect postgres1 --format '{{ (index .Mounts 0).Source }}' ) && echo $FILES
 
 
 
-# get container root location
-FILES=$(docker inspect sqlcontainer1 --format '{{ .GraphDriver.Data.MergedDir }}') && echo $FILES
-
-
-
-# view root directory of the container on the host
-sudo ls -al $FILES
-
-
-
-# view the database files on the host
+# view view contents of the volume on the host
+sudo ls -al $FILES/base
 
 
 
 # clean up
 docker rm $(docker ps -aq) -f
-
-
-
-# ok, so if we know the constructs around a container...can we build one from scratch?
-# yes! yes we can
-# first, run a container with docker (yes, I know but just wait :-) )
-docker container run -d \
---publish 15800:1433 \
---env ACCEPT_EULA=Y \
---env MSSQL_SA_PASSWORD=Testing1122 \
---name sqlcontainer3 \
-mcr.microsoft.com/mssql/server:2019-CU5-ubuntu-18.04
-
-
-
-# confirm the container is running
-docker container ls -a --format "table {{.Names }}\t{{ .Image }}\t{{ .Status }}\t{{.Ports}}"
-
-
-
-# let's have a look at the logs in the container as well (to make sure that SQL is running)
-docker logs sqlcontainer3
-
-
-
-# stop the container
-docker stop sqlcontainer3
-
-
-
-# export the container to a .tar file
-docker export sqlcontainer3 -o ~/sqlserver.tar
-
-
-
-# now extract the .tar file to a directory
-mkdir ~/sqlserver
-tar -xvf ~/sqlserver.tar -C ~/sqlserver
-
-
-
-# list the contents of that directory
-ls ~/sqlserver
-
-
-
-# let's add a file to that root directoy
-touch ~/sqlserver/HI_DBAFROMTHECOLD
-
-
-
-# let's start up that container again
-docker start sqlcontainer3
-
-
-
-# list the root directory contents in the container
-docker exec sqlcontainer3 ls /
-
-
-
-# blow that container away (we don't need it anymore)
-docker rm sqlcontainer3 -f
-
-
-
-# navigate to some code
-cd ~/git/dockerdeepdive/Demos/ContainerFromScratch/
-
-
-
-# view the code
-cat main.go
-
-
-
-# open a tmux session and split the command prompt (ctrl+B then ")
-tmux
-
-
-
-# spin up the container using Liz Rice's containers from scratch code...
-# https://www.youtube.com/watch?v=_TsSmSu57Zo
-sudo go run main.go run /bin/bash
-
-
-
-# check the root directory
-ls /
-
-
-
-# now check the hostname
-hostname
-
-
-
-# add the urandom file - thanks Mark!
-mknod -m 444 /dev/urandom c 1 9
-
-
-
-# confirm file is there
-ls /dev
-
-
-
-# spin up SQL Server in the background
-/opt/mssql/bin/sqlservr&> /dev/null &
-
-
-
-# check processes in the container
-ps aux
-
-
-
-# check process on the host
-ps aux | grep mssql
-
-
-
-# have a look at the memory and cpu control groups location (notice sqlserver folder)
-ls /sys/fs/cgroup/memory
-ls /sys/fs/cgroup/cpu,cpuacct
-
-
-
-# get memory limit
-MEMORYLIMIT=$(cat /sys/fs/cgroup/memory/sqlserver/memory.limit_in_bytes)
-
-
-
-# show memory limit of container in MB
-expr $MEMORYLIMIT / 1024 / 1024
-
-
-
-# show CPU limit
-cat /sys/fs/cgroup/cpu,cpuacct/sqlserver/cpu.cfs_quota_us
-
-
-
-# exit out of tmux and container
-exit
---
+++ demos/3.NamedVolumes.sh
@@ -0,0 +1,103 @@
+############################################################################
+############################################################################
+#
+# Docker & Databases - Andrew Pruski
+# @dbafromthecold
+# dbafromthecold@gmail.com
+# https://github.com/dbafromthecold/DockerAndDatabases
+# Named Volumes
+#
+############################################################################
+############################################################################
+
+
+
+# confirm no containers running
+docker container ls -a
+
+
+
+# create a named volume
+docker volume create postgres-data
+
+
+
+# confirm volume
+docker volume ls
+
+
+
+# run a container with a named volume
+docker container run -d \
+--name postgres1 \
+--publish 15791:5432 \
+--env POSTGRES_PASSWORD=Testing1122 \
+--volume postgres-data:/var/lib/postgresql/data \
+postgres:17
+
+
+
+# confirm container is running
+docker container ls -a
+
+
+
+# inspect the container
+# https://docs.docker.com/engine/storage/bind-mounts/#configure-bind-propagation
+docker container inspect postgres1  | jq '.[0].Mounts'
+
+
+
+# set environmant variable for password
+export PGPASSWORD='Testing1122'
+
+
+
+# confirm connection to postgres
+psql -h localhost -p 15791 -U postgres -d postgres -c "SELECT version();"
+
+
+
+# create a database
+psql -h localhost -p 15791 -U postgres -d postgres -c "CREATE DATABASE testdatabase"
+
+
+
+# confirm database has been created
+psql -h localhost -p 15791 -U postgres -d postgres -l
+
+
+
+# stop and remove the container
+docker container rm postgres1 -f
+
+
+
+# confirm no containers running
+docker container ls -a
+
+
+
+# create volume still exists
+docker volume ls
+
+
+
+# run another container with the same named volume
+docker container run -d \
+--name postgres2 \
+--publish 15792:5432 \
+--env POSTGRES_PASSWORD=Testing1122 \
+--mount type=volume,src=postgres-data,dst=/var/lib/postgresql/data,volume-driver=local \
+postgres:latest
+
+
+
+# confirm custom database is still there
+psql -h localhost -p 15792 -U postgres -d postgres -l
+
+
+
+# clean up
+docker container rm postgres2 -f
+docker volume rm postgres-data
